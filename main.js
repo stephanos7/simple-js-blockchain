55353d5f14618312899e1620152f15420920f292
Stephanos Theodotou <stephanos.theodotou@gmail.com>
2018-09-21 12:16:53 +0100

    add block class properties including a new hash property as empy string. Add calculateBlockHash method by concatinating the block class properties and stringifing

--- main.js
+++ main.js
@@ -1,15 +1,25 @@
 // stephanosCoin
+// based on 'Building a Blockchain' by Kore on
+// https://medium.com/@akshaykore/building-a-blockchain-7579c53962dd
 
 class Block {
 
   // index [optional] = position of block in the chain
   // timestamp        = time stamp against the creation of the block
   // data             = payload values 
-  // previous Hash    = String of the previous block's hash value
+  // prevHash    = String of the previous block's hash value
 
-  constructor(index, timestamp, data, previousHash = ""){
+  constructor(index, timestamp, data, prevHash = ""){
+    this.index = index;
+    this.timestamp = timestamp;
+    this.data = data;
+    this.prevHash = prevHash;
 
+    // unique hash property for the block set as empty
+    this.hash = "";
   }
 
-
+  calculateBlockHash(){
+    return this.hash = (this.index + this.timestamp + JSON.stringify(this.data) + this.prevHash).toString();
+  }
 }
