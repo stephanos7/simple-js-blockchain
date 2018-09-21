c44b3917015f7941f464bcaf811b4d5dd3cb3b00
Stephanos Theodotou <stephanos.theodotou@gmail.com>
2018-09-21 12:08:26 +0100

    define block class with constructor and arguments

---
+++ main.js
@@ -0,0 +1,15 @@
+// stephanosCoin
+
+class Block {
+
+  // index [optional] = position of block in the chain
+  // timestamp        = time stamp against the creation of the block
+  // data             = payload values 
+  // previous Hash    = String of the previous block's hash value
+
+  constructor(index, timestamp, data, previousHash = ""){
+
+  }
+
+
+}
